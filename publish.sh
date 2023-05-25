6be514afa5f9669c4fe8906801d6ff1bcd28b003
Andrei <nzpopa@users.noreply.github.com>
2023-05-25 16:00:08 +0300

    added publish

---
+++ publish.sh
@@ -0,0 +1,12 @@
+npm version patch && \
+    bash build.sh && \
+    rm -rf to-npm && \
+    mkdir -p to-npm/dist
+    cp dist/* to-npm/dist && \
+    cp package.json to-npm && \
+    cd to-npm && npm pack && \
+    git add . && \
+    git commit -m "release $(npm pkg get version | tr -d '\"')" && \
+    git push && \
+    gh release create v$(npm pkg get version | tr -d '"') --generate-notes ./*.tgz && \
+    npm publish --access public --tag v$(npm pkg get version | | tr -d '"')
